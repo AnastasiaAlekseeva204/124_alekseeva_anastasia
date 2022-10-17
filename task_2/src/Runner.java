0cc3537548090de0ef513cdb6118041fe116668d
Anastasia Alekseeva <112937401+AnastasiaAlekseeva204@users.noreply.github.com>
2022-10-18 00:04:35 +0300

    done

--- task_2/src/Runner.java
+++ task_2/src/Runner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Р’РІРµРґРёС‚Рµ СЃРІРѕРµ С„Р°РјРёР»РёСЋ Рё РёРјСЏ");
+        System.out.println("Введите свое фамилию и имя");
         String name = scanner.nextLine();
         Task_2 task = new Task_2();
         Task_2_Tests.test(task, name);
