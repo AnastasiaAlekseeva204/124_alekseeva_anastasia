bc70a2593163d119ec969129947b72a466625866
Anastasia Alekseeva <112937401+AnastasiaAlekseeva204@users.noreply.github.com>
2022-10-18 15:03:22 +0300

    done

--- task_3/src/Runner.java
+++ task_3/src/Runner.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Р’РІРµРґРёС‚Рµ СЃРІРѕРµ С„Р°РјРёР»РёСЋ Рё РёРјСЏ");
+        System.out.println("Введите свое фамилию и имя");
         String name = scanner.nextLine();
         Task_3 task = new Task_3();
         Task_3_Tests.test(task, name);
--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -1,14 +1,44 @@
 public class Task_3 implements Task_3_base {
     @Override
     public int subtask_1_for(int n1, int n2, int a, int b) {
-        // подсчитать, сколько чисел, кратных a, но не кратных b,
+        int t=0;
+        if (n1>n2){
+            while (n1-n2>=1){
+                n1=n1-1;
+                if ((n1%a==0) && (n1%b!=0)){
+                    t=t+1;
+                }
+                else{
+                    t= t+0;
+                }
+            }
+        }
+        else{
+            while (n2-n1>=1){
+                n2=n2-1;
+                if ((n2%a==0) && (n2%b!=0)){
+                    t=t+1;
+                }
+                else{
+                    t= t+0;
+                }
+            }
+
+        }
+        return t;
+    } // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        return 0;
-    }
 
     @Override
     public int subtask_2_for(int num) {
-        // Последовательность чисел строится следующим образом:
+        int p = 0;
+        int t = 1;
+        for (int i = 0; i <= num; i++) {
+            p++;
+            for (int l = 0; l < t; l++)
+                i++;
+            t++;
+        }   // Последовательность чисел строится следующим образом:
         // сначала идет одна единица,
         // потом две двойки,
         // потом три тройки,
@@ -17,24 +47,35 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        return 0;
+        return p;
     }
 
     @Override
     public int subtask_3_for(int num, int d, int cnt) {
-        // Дана последовательность
+        int a = num;
+        int i = 0;
+        for (int o = 0; o < cnt; o++){
+            i = i + a;
+            a = a*d + 1;
+        }
+        return i;
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
     }
 
     @Override
     public int subtask_4_for(int n) {
-        // Вычислить сумму
+        int v = 1;
+        int z = 0;
+        for(int f = 1; f <= n; f++) {
+            v = v * f;
+            z = z + v;
+        }
+        return z; // Вычислить сумму
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
     }
 }
+
