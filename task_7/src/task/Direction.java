1b8c658ef3cd076738ef4fdf8008789e4fff8a70
Anastasia Alekseeva <112937401+AnastasiaAlekseeva204@users.noreply.github.com>
2023-01-05 12:23:33 +0300

    new

--- task_7/src/task/Direction.java
+++ task_7/src/task/Direction.java
@@ -3,13 +3,13 @@ package task;
 public enum Direction {
     North(0, -1), West(-1, 0), South(0, 1), East(1, 0);
 
-    private int X;
-    private int Y;
-    Direction(int X, int Y){
-        this.X = X;
-        this.Y = Y;
+    private int x;
+    private int y;
+    Direction(int x, int y){
+        this.x = x;
+        this.y = y;
     }
     public Vector2 move(Vector2 begining, double interval){
-        return new Vector2(begining.getX() + (interval * X), begining.getY() + (interval * Y));
+        return new Vector2(begining.getX() + (interval * x), begining.getY() + (interval * y));
     }
 }
