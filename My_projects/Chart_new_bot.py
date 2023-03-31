6743a7611413e10151bdced79b9f2a2217595a01
Anastasia Alekseeva <112937401+AnastasiaAlekseeva204@users.noreply.github.com>
2023-04-01 01:15:15 +0300

    new(chart_bot)

--- My_projects/Chart_new_bot.py
+++ My_projects/Chart_new_bot.py
@@ -19,7 +19,9 @@ def subjects(update, context):
 # Функция для обработки команды /advice
 def advice(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Несколько советов для успешной подготовки к поступлению: \n 1. Начните готовиться заранее, не откладывайте на потом \n 2. Составьте план подготовки, распределяя время на каждый предмет \n 3. Используйте различные источники для подготовки, не ограничивайтесь только учебниками \n 4. Решайте много тестов и задач, чтобы привыкнуть к формату экзамена \n 5. Поддерживайте здоровый образ жизни, не забывайте о физических упражнениях и здоровом питании")
-
+# Функция для обработки команды /actual
+def actual(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Список актуальных профессий связаных с предметами: \n 1.Робототехника и киберфизические системы(предметы: математика,русский язык и химия) \n 2.Геология и геохимия горючих ископаемых(предметы: математика,русский язык и химия) \n 3.Data Mining Specialist: специалист по интеллектуальной обработке данных(предметы: математика,русский язык и биология)")
 # Функция для обработки всех остальных сообщений
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Извините, я не понимаю эту команду. Пожалуйста, введите /help")
@@ -33,6 +35,7 @@ dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("help", help))
 dispatcher.add_handler(CommandHandler("subjects", subjects))
 dispatcher.add_handler(CommandHandler("advice", advice))
+dispatcher.add_handler(CommandHandler("actual",actual))
 
 # Регистрируем обработчик всех остальных сообщений
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
