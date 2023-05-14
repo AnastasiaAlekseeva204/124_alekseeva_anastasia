ecd2ae729df4707bef08fb64d2c2ddcb94160ce1
Anastasia Alekseeva <112937401+AnastasiaAlekseeva204@users.noreply.github.com>
2023-05-14 15:09:39 +0300

    new

--- js/p5js/My_game/script.js
+++ js/p5js/My_game/script.js
@@ -1,8 +1,77 @@
 var px = 21
 var delta = 1;
 var py = 370;
+var sp;
 let timer;
 let fn;
+let scor;
+let rects = [];
+let countrect = 5;
+var scolor = ['#a24db2','#421f9c','#a61d15','black','white','#b11b51'];
+function setup(){
+	createCanvas(1000,500);
+	background('#abe2e9');
+	timer = new timing();
+	fn  = new fon();
+	scor = new scoring();
+//	r = new specrec();
+    for(let i = 0; i<countrect;i++){
+		let myr = rectob.init();
+		let col = round(random(0,5));
+		console.log(col);
+		rects.push(new rectob(myr.x,myr.y,40,40,col));
+	}
+	console.log(rects);
+}
+function draw(){
+	background('#abe2e9');
+	timer.show();
+	fn.showBg();
+	fn.showSun();
+	
+	noStroke();
+	fill('#e519bd');
+	ellipse(px,py,40,60);
+	px = px+delta;
+	if (px >= 980 || px <=20){
+		delta = -delta;
+	}
+	for(let i = 0; i<countrect; i++){
+		rects[i].display();
+	}
+	//r.show();
+}
+class rectob{
+	constructor(x,y,w,h,col){
+		this.x = x;
+		this.y = 360;
+		this.w = w;
+		this.h = h;
+		this.col = col;
+	}
+	display(){
+		fill(scolor[this.col]);
+		rect(this.x,this.y,this.w,this.h);
+	}
+	static init(){
+		return {
+			x: random(50,950),
+			y:360,
+			w:40,
+			h:40,
+		}
+	}
+}
+class scoring{
+	constructor(){
+    }
+	show(){
+		noStroke();
+		fill('black');
+		textSize(20);
+		s = 'Счет: ';
+	}
+}
 class timing {
 	constructor(){
 		this.t = 0;
@@ -27,6 +96,7 @@ class fon{
 		line(875,60,920,60);
 		line(750,60,805,60);
 		line(840,90,840,135);
+		line(840,10,840,30);
 	}
 	showBg(){
 		stroke('black');
@@ -62,27 +132,7 @@ class fon{
 		ellipse(757,250,100,100);
 	}
 }
-function setup(){
-	createCanvas(1000,500);
-	background('#abe2e9');
-	timer = new timing();
-	fn  = new fon();
-}
-function draw(){
-	background('#abe2e9');
-	timer.show();
-	fn.showBg();
-	fn.showSun();
-	
-	noStroke();
-	fill('#e519bd');
-	ellipse(px,py,40,60);
-	px = px+delta;
-	if (px >= 980 || px <=20){
-		delta = -delta;
-	}
-	
-}
+
 function mousePressed() {
 	py = py-50;
 }
